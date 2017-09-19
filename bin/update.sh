443779ad064012651f23d9d1559cffcb7653aa5c
Knut Kohl <github@knutkohl.de>
2017-09-19 10:55:37 +0200

    Fix path to update.sh for inital download

--- bin/update.sh
+++ bin/update.sh
@@ -14,7 +14,7 @@ eval $(grep -v '^;' $cfg | sed 's/ *\(=\) */\1/g')
 
 ### Temp file for download
 new=$(mktemp)
-trap "rm $new >/dev/null 2>&1" 0
+trap 'rm $new >/dev/null &>/dev/null' 0
 
 ### Fetch latest Adminer for MySQL only
 wget -qO $new http://www.adminer.org/$version
@@ -22,12 +22,10 @@ wget -qO $new http://www.adminer.org/$version
 ### Check if the download was successful
 ### (sometimes the downlaoded file is empty, I think it happens on timeouts)
 if [ -s $new ]; then
-
     ### Change attributes and owner to the same as index.php
     chmod --reference=$path/index.php $new
     chown --reference=$path/index.php $new
 
     ### Move the just downloaded file to its correct name
     mv $new $path/adminer.php
-
 fi
