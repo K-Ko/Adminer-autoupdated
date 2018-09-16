a12d732cf4efc61408943b160f00076ce4f03da1
Knut Kohl <github@knutkohl.de>
2018-09-16 12:52:58 +0200

    Fix wrong working path

--- bin/update.sh
+++ bin/update.sh
@@ -16,18 +16,20 @@ eval $(grep -v '^;' $cfg | sed 's/ *\(=\) */\1/g')
 new=$(mktemp)
 trap 'rm $new >/dev/null &>/dev/null' 0
 
+cd $path
+
 ### Fetch latest Adminer for MySQL only
 wget -qO $new http://www.adminer.org/$version
 
 ### Check if the download was successful
 ### (sometimes the downlaoded file is empty, I think it happens on timeouts)
 if [ -s $new ]; then
-    ### Change attributes and owner to the same as index.php
-    chmod --reference=$path/index.php $new
-    chown --reference=$path/index.php $new
-
     ### Move the just downloaded file to its correct name
-    mv $new $path/adminer.php
+    mv $new adminer.php
+
+    ### Change attributes and owner to the same as index.php
+    chmod --reference=index.php adminer.php
+    chown --reference=index.php adminer.php
 fi
 
 ### Update sub modules
